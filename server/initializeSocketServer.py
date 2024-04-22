3ef78b1c26322bdb753aca58b20d11f3f30b19a2
longnguyen <chanhlong171002@gmail.com>
2024-04-22 14:42:56 +0700

    Feat: Create the SocketServer(connected, communicated from server to client and opposite) .

---
+++ server/initializeSocketServer.py
@@ -0,0 +1,29 @@
+import socket
+from conf.readConf import load_data_server
+
+
+def initialize_socket_server():
+    host, port = load_data_server("../conf/config.json")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_server:
+        socket_server.bind((host, port))
+        socket_server.listen(2)
+        """
+            connect has the structure {laddr=('127.0.0.1', 65432)}, this is the location of server.
+            And address has the structure {laddr=('127.0.0.1', x)}, x mean the random port in our computer to use it for
+            the client. "x" can be "41888" or "42562",... 
+        """
+        connect, address = socket_server.accept()
+        print(connect, address)
+        print("Connected by", address)
+        with connect:
+            while True:
+                data = connect.recv(1024).decode()
+                if not data:
+                    break
+                print(f"From connected user {address[1]}: " + str(data))
+                data = input(" -->")
+                connect.sendall(data.encode())
+
+
+if __name__ == "__main__":
+    initialize_socket_server()
