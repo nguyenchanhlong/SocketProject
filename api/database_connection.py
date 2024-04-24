e813e0bdee909edcfc0443e25d5c07891bbf8fd0
nguyenchanhlong <chanhlong171002@gmail.com>
2024-04-24 14:37:51 +0700

    Fix: Change location and small thing about the structure (in the past I used config file to take host,port,..., and I change to use .env to take those items).

--- api/database_connection.py
+++ api/database_connection.py
@@ -3,32 +3,27 @@ from typing import Any, Mapping
 from pymongo import MongoClient
 from pymongo.collection import Collection
 
-from services.conf.readConf import load_data_db
-
-host_conf, port_conf, username_conf, password_conf, db_conf, collection_conf = load_data_db("../conf/config.json")
+from settings import settings
 
 
 def _connect_db(host: str,
                 port: int,
                 username: str,
                 password: str,
-                db: str,
-                collection: str) -> MongoClient[Mapping[str, Any] | Any]:
+                db: str) -> MongoClient[Mapping[str, Any] | Any]:
     if username and password:
         mongo_uri = f"mongodb://{username}:{password}@{host}:{port}"
         conn = MongoClient(mongo_uri)
     else:
         conn = MongoClient(host=host, port=port)
     database = conn.get_database(db)
-    get_collection = database.get_collection(collection)
-    return get_collection
+    return database
 
 
 mongo_connection: Collection[Mapping[str, Any] | Any] = _connect_db(
-    host=host_conf,
-    port=port_conf,
-    username=username_conf,
-    password=password_conf,
-    db=db_conf,
-    collection=collection_conf
+    host=settings.DATABASE_HOST,
+    port=settings.DATABASE_PORT,
+    username=settings.DATABASE_USERNAME,
+    password=settings.DATABASE_PASSWORD,
+    db=settings.DB,
 )
